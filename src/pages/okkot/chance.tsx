407a2299e7c08deb005e9cf7a4ae7e19ab17c1a7
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-12-25 01:51:20 +0900

    fix: ssr test with new page

---
+++ src/pages/okkot/chance.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import Layout from "../../components/Layout";
+
+const Test1 = styled.div`
+  font-size: 32px;
+  font-weight: 600;
+`;
+
+const Test2 = styled.div`
+  background-color: yellow;
+  padding: 20px;
+`;
+
+const Test3 = styled.div`
+  font-size: 48px;
+  color: blue;
+`;
+
+const Test4 = styled.div`
+  font-size: 20px;
+  letter-spacing: 12px;
+`;
+
+const Chance = () => {
+  return (
+    <Layout>
+      <Test1>왜깜빡이냐고</Test1>
+      <Test2>개빡치게</Test2>
+      <Test3>적당히하라고</Test3>
+      <Test4>해달라는거다해줬잖아</Test4>
+    </Layout>
+  );
+};
+
+export default Chance;
