2830d117c7fcab014ab005d803aca96125b9a7a7
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-12-25 01:36:32 +0900

    fix: ssr font-test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,7 +24,15 @@ class MyDocument extends Document<IProps> {
   render() {
     return (
       <Html>
-        <Head>{this.props.styleTags}</Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
+          <script src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script>
+          {this.props.styleTags}
+        </Head>
         <body>
           <Main />
           <NextScript />
