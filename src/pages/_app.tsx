0dff837e208285c5837b8cb7e6c3b3d21b5cf997
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-12-25 01:11:07 +0900

    fix: change _app.tsx(change gs literal)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,31 @@
 import React, { Fragment } from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { createGlobalStyle } from "styled-components";
 import Header from "../components/Header";
 
-const GlobalStyles = createGlobalStyle`
-    * {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-    }
-    body {
-        font-family: 'Noto Sans KR', sans-serif;
-    }
-    a {
-        color: inherit;
-        text-decoration: none;
-    }
-    #__next {
-        height: 100%;
-    }
-`;
+const globalStyle = `
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Noto Sans KR', sans-serif;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+#__next {
+  height: 100%;
+}`;
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Fragment>
-      <GlobalStyles />
       <Head>
         <title>Hello, OKKOT</title>
+        <style>{globalStyle}</style>
       </Head>
       <Header />
       <Component {...pageProps} />
