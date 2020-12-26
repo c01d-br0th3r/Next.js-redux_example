973cdca72b261fe7f703f555fa725a062f821029
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-12-27 02:35:59 +0900

    next deploy test: remove stack

--- src/pages/okkot/best.tsx
+++ src/pages/okkot/best.tsx
@@ -1,13 +1,11 @@
 import React, { Fragment } from "react";
 
-import StackNavigation from "../../components/StackNavigation";
 import Label from "../../components/Label";
 
 const Best = () => {
   return (
     <Fragment>
       <Label>Best 페이지입니다.</Label>
-      <StackNavigation />
     </Fragment>
   );
 };
--- src/pages/okkot/new.tsx
+++ src/pages/okkot/new.tsx
@@ -1,13 +1,11 @@
 import React, { Fragment } from "react";
 
-import StackNavigation from "../../components/StackNavigation";
 import Label from "../../components/Label";
 
 const New = () => {
   return (
     <Fragment>
       <Label>new 페이지입니다.</Label>
-      <StackNavigation />
     </Fragment>
   );
 };
