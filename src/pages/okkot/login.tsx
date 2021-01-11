61899fbf8f7504a0abe040da175c72018c08c792
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2021-01-11 16:54:16 +0900

    feat: Emergency...

--- src/pages/okkot/login.tsx
+++ src/pages/okkot/login.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { Button, Label, TextField } from "@okkot/ui";
 
 const Login = () => {
-  return (
-    <div>
-      <Button width="100%" size="M" label="로그인" />
-      <Label>안녕하세요</Label>
-      <TextField width="100%" />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Login;
