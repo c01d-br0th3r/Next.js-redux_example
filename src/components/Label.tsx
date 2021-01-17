814dc026676eb23cf9cf914013393ebf60f782e7
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2021-01-17 11:57:43 +0900

    chore(atoms/Label): letterSpacing 프로퍼티 추가

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -11,17 +11,19 @@ interface ILabelProps {
   backgroundColor?: string;
   onClick?: React.MouseEventHandler<HTMLElement>;
   id?: string;
+  letterSpacing?: string;
 }
 
 const StyledLabel = styled.div<ILabelProps>`
   font-size: ${(props) => (props.size ? props.size : "100%")};
-  font-weight: ${(props) => (props.weight ? props.weight : "120px")};
+  font-weight: ${(props) => (props.weight ? props.weight : "400")};
   color: ${(props) => (props.color ? props.color : "#232323")};
   margin: ${(props) => props.margin && props.margin};
   background-color: ${(props) =>
     props.backgroundColor && props.backgroundColor};
   padding: ${(props) => props.padding && props.padding};
   border-radius: 2px;
+  letter-spacing: ${(props) => props.letterSpacing && props.letterSpacing};
 `;
 
 const Label: React.FC<ILabelProps> = (props) => {
